db95581fb96242997d2ac4be89d64daf1788a5d0
julia zieba <maybe.nott123@gmail.com>
2019-11-15 20:10:38 +0000

    created CSG_C3_C1 directory

---
+++ CGS_C3_C1/shell.c
@@ -0,0 +1,12 @@
+// CSG_C3_C1
+#include <stdio.h>
+#include <string.h>
+#include "filesys.h"
+
+int main()
+{
+    format();
+    writedisk("virtualdiskC3_C1");
+    // printBlock();
+    return 0;
+}
